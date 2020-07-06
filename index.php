8a2d4b5e3a304dedde94ed3409d3ff5d79d90ebf
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-06 15:23:57 +0900

    ページネーション

--- index.php
+++ index.php
@@ -43,6 +43,19 @@ $memos->execute();
         <time><?php print($memo['created_at']); ?></time>
         <hr>
     <?php endwhile ?>
+    <?php if ($page >= 2): ?>
+        <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1) ?> ページ目へ</a>
+    <?php endif; ?>
+    ||
+    <?php
+        $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+        $count = $counts->fetch();
+        $max_page = ceil($count['cnt'] / 5);
+        if ($page < $max_page) :
+            ?>
+        <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1) ?> ページ目へ</a>
+    <?php
+        endif; ?>
 </article>
 </main>
 </body>    

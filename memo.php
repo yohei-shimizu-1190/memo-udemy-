0bd18e3bca8fa1de1ccef7334b14b2241afb923b
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-06 16:17:41 +0900

    削除機能

--- memo.php
+++ memo.php
@@ -35,6 +35,7 @@ $memo = $memos->fetch();
 <article>
   <pre> <?php print($memo['memo']); ?> </pre>
   <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
+  <a href="delete.php?id=<?php print($memo['id']); ?>">削除する</a>
   <a href="index.php">戻る</a>
 </article>
 </main>
